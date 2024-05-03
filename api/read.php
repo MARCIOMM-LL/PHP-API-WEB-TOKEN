4b7fb25d5bdc3cc56ce61a5d73d12b6ec0d29acd
Andrew <32685719+MARCIOMM-LL@users.noreply.github.com>
2024-05-03 17:20:09 -0300

    Update read.php
    Hypertext Transfer Protocol Version 2 (HTTP/2)

--- api/read.php
+++ api/read.php
@@ -22,6 +22,6 @@ if ($requestMethod === "GET") {
         'status' => 405,
         'message' => $requestMethod . ' Método não permitido!',
     ];
-    header("HTTP/1.0 405 Método não permitido!");
+    header("HTTP/2 405 Método não permitido!");
     return json_encode($data);
 }
